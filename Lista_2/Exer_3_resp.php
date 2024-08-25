46376c0b6459d2ca3d5edec17a65222d8a4aa3da
Felipe Pretti <felipepretti2001@gmail.com>
2024-08-25 11:16:42 -0300

    Exer_3+Ajuste_Exer_2

--- Lista_2/Exer_3_resp.php
+++ Lista_2/Exer_3_resp.php
@@ -3,19 +3,19 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Exercício 2 : Resposta</title>
+    <title>Exercício 3 : Resposta</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Resposta do exercício 2</h1>
+    <h1>Resposta do exercício 3</h1>
     <?php
         if($_SERVER["REQUEST_METHOD"] == "POST"){
             try{
                 $valor1 = (int) $_POST['valor1'] ?? 0;
                 $valor2 = (int) $_POST['valor2'] ?? 0;
                 
-                $resultado = $valor1 - $valor2;
-                echo "<p>Subtração: $resultado </p>";
+                $resultado = $valor1 * $valor2;
+                echo "<p>Multiplicação: $resultado </p>";
 
             } catch(Exception $e){
                 echo "ERRO! ".$e->getMessage();
