e7e055e36bbd965e0a1366e1008106c0ac1350b6
Felipe Pretti <felipepretti2001@gmail.com>
2024-08-25 14:02:56 -0300

    Exer_6

---
+++ Lista_2/Exer_6.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Lista 2</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body class="cointainer m-5">
+    <h1>Exercicio 6</h1>
+    <form action="Exer_6_resp.php" method="POST">
+    <div class="row g-3 align-items-center">
+        <div class="col-auto">
+            <label class="form-label" for="celsius">Valor em Celsius (°C)</label>
+        </div>
+        <div class="col-1">
+            <input class="form-control" name="celsius" id="celsius">
+        </div>
+    </div>
+    <div class="row pt-3">
+            <div class="col">
+                <button type="submite" class="btn btn-primary">Converter</button>
+            </div>
+        </div>
+    </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
