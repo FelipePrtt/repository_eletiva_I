29344c614a7f14537d1a128ce4fbabf9c4c2ce47
FelipePrtt <128757278+FelipePrtt@users.noreply.github.com>
2024-08-27 21:47:38 -0300

    Exer_1_inicio

---
+++ Lista_3/Exer_1.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Lista 3</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1>Exercício 1</h1>
+    <form action="Exer_1_resp.php" method="POST" class="m-4">
+        <div class="row mt-4">
+            <div class="col">
+                <label for="n1" class="form-label">Informe o primeiro valor</label>
+                <div class="col-1">
+                    <input type="number" id="n1" name="n1" placeholder="1° valor..." class="form-control">
+                </div>
+            </div>
+        </div>
+        <div class="row mt-4">
+            <div class="col">
+                <label for="n2" class="form-label">Informe o segundo valor</label>
+                <div class="col-1">
+                    <input type="number" id="n2" name="n2" placeholder="2° valor..." class="form-control">
+                </div>
+            </div>
+        </div>
+        <div class="row mt-4">
+            <div class="col">
+                <label for="n3" class="form-label">Informe o terceiro valor</label>
+                <div class="col-1">
+                    <input type="number" id="n3" name="n3" placeholder="3° valor..." class="form-control">
+                </div>
+            </div>
+        </div>
+        <div class="row mt-4">
+            <div class="col">
+                <label for="n4" class="form-label">Informe o quarto valor</label>
+                <div class="col-1">
+                    <input type="number" id="n4" name="n4" placeholder="4° valor..." class="form-control">
+                </div>
+            </div>
+        </div>
+        <div class="row mt-4">
+            <div class="col">
+                <label for="n5" class="form-label">Informe o quinto valor</label>
+                <div class="col-1">
+                    <input type="number" id="n5" name="n5" placeholder="5° valor..." class="form-control">
+                </div>
+            </div>
+        </div>
+        <div class="row mt-4">
+            <div class="col">
+                <label for="n6" class="form-label">Informe o sexto valor</label>
+                <div class="col-1">
+                    <input type="number" id="n6" name="n6" placeholder="6° valor..." class="form-control">
+                </div>
+            </div>
+        </div>
+        <div class="row mt-4">
+            <div class="col">
+                <label for="n7" class="form-label">Informe o sétimo valor</label>
+                <div class="col-1">
+                    <input type="number" id="n7" name="n7" placeholder="7° valor..." class="form-control">
+                </div>
+            </div>
+        </div>
+        <div class="row mt-4">
+            <div class="col-2">
+                <button type="submite" class="btn btn-primary">Enviar</button>
+            </div>
+        </div>
+    </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
