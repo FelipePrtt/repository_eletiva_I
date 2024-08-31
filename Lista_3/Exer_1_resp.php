553a30204ff849043cdadfc6ae13f2a2225f4097
FelipePrtt <felipepretti2001@gmail.com>
2024-08-31 10:30:49 -0300

    Exer_1Correcao

--- Lista_3/Exer_1_resp.php
+++ Lista_3/Exer_1_resp.php
@@ -7,38 +7,27 @@
 </head>
 <body>
     <?php
-        if ($_SERVER['REQUEST_METHOD'] == "POST") 
+        if ($_SERVER['REQUEST_METHOD'] == 'POST')
+        try
         {
-            try 
+            $menor = PHP_INT_MAX;
+            $menor_pos = 0;
+
+            for ($i=1; $i <= 7; $i++)
             {
-                $n1 = (int)$_POST['n1'] ?? 0;
-                $n2 = (int)$_POST['n2'] ?? 0;
-                $n3 = (int)$_POST['n3'] ?? 0;
-                $n4 = (int)$_POST['n4'] ?? 0;
-                $n5 = (int)$_POST['n5'] ?? 0;
-                $n6 = (int)$_POST['n6'] ?? 0;
-                $n7 = (int)$_POST['n7'] ?? 0;
-
-                $numeros = array($n1, $n2, $n3, $n4, $n5, $n6, $n7);
-
-                $menor = $numeros[0];
-                $menor_pos = 1;
-
-                for ($i = 1; $i < 7; $i++) 
+                $valor = $_POST["n$i"];
+                if ($valor < $menor)
                 {
-                    if ($numeros[$i] < $menor) 
-                    {
-                        $menor = $numeros[$i];
-                        $menor_pos = $i+1;
-                    }
+                    $menor = $valor;
+                    $menor_pos = $i;
                 }
-                
-                echo "<p>O menor valor informado é $menor, e foi inserido na posição $menor_pos.</p>";
-                
-            } catch (Exception $e) 
-            {
-                echo ("Erro: $e");
+
             }
+            echo "<p>Menor valor --->$menor</p>";
+            echo "<p>Posição do menor valor --->$menor_pos</p>";
+        }catch (Exception $e)
+        {
+            echo "Erro:".$e->getMessage();
         }
     ?>
 </body>
