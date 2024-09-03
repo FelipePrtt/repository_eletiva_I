bcb22b4d30806a47ae36bb0bea449820f0aad28c
FelipePrtt <128757278+FelipePrtt@users.noreply.github.com>
2024-09-03 20:37:10 -0300

    Exer_1 (incompleto)

---
+++ Lista_4/Exer_1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="Exer_1_resp.php" method="post">
+        <div class="row">
+            <div class="col">
+                <label for="palavra" class="form-label">Informe uma palavra</label>
+                <input type="text" name="palavra" id="palavra" class="form-control">
+            </div>
+            <div class="col">
+                <button type="submit" class="btn btn-primary">Enviar</button>
+            </div>
+        </div>
+    </form>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
