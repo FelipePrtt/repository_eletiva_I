70abb7e116d5600165367d39378754d6481f475d
Felipe Pretti <felipepretti2001@gmail.com>
2024-09-08 22:25:19 -0300

    Exer_3(Ajuste)

--- Lista_4/Exer_3_resp.php
+++ Lista_4/Exer_3_resp.php
@@ -1,3 +1,7 @@
+<?php
+    declare(strict_types = 1);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,19 +11,24 @@
 </head>
 <body>
     <?php
+        function verificar_contencao(string $tx1, string $tx2):void{
+            if (strpos($tx1, $tx2) !== false)
+            {
+                echo "<p>A palavra $tx2 está contida em $tx1.</p>";
+            }
+            else
+            {
+                echo "<p>A palavra $tx2 não está contida em $tx1.</p>";
+            } 
+        }
+
         if ($_SERVER['REQUEST_METHOD'] == 'POST')
         try
         {
             $texto1 = $_POST['texto1'];
             $texto2 = $_POST['texto2'];
-            if (strpos($texto1, $texto2) !== false)
-            {
-                echo "<p>A palavra $texto2 está contida em $texto1.</p>";
-            }
-            else
-            {
-                echo "<p>A palavra $texto2 não está contida em $texto1.</p>";
-            }    
+            
+            echo verificar_contencao($texto1, $texto2);
         }catch (Exception $e)
         {
             echo "Erro:".$e->getMessage();
