71e0b5e9715e6aff31683bbaebe3377168094b63
Felipe Pretti <felipepretti2001@gmail.com>
2024-10-29 23:46:03 -0300

    CRUD do usuário

--- projetobdviews/funcoes/usuario.php
+++ projetobdviews/funcoes/usuario.php
@@ -6,7 +6,7 @@ require_once('../config/bancodedados.php');
 
 function login(string $email, string $senha)
 {
-    global $pdo; //"global" permite usar qualque variavel global em outra classe.
+    global $pdo; //"global" permite usar qualquer variavel global em outra classe.
 
     //iserção do usuário adm
     $stament = $pdo->query("SELECT * FROM usuario WHERE email = 'adm@adm.com'");
@@ -34,4 +34,35 @@ function login(string $email, string $senha)
     {
         return null; 
     }
+}
+
+function novoUsuario(string $nome, string $email, string $senha, string $nivel):bool
+{
+    global $pdo;
+    $senha_criptografada = password_hash($senha, PASSWORD_BCRYPT);
+    $stament = $pdo->prepare("INSERT INTO usuario (nome, email, senha, nivel) VALUES (?, ?, ?, ?)");
+    return $stament->execute([$nome, $email, $senha_criptografada, $nivel]);
+}
+
+function excluirUsuario(int $id):bool
+{
+    global $pdo;
+    $stament = $pdo->prepare("DELETE FROM usuario WHERE id = ?");
+    return $stament->execute([$id]);
+}
+
+function todosUsuario():array
+{
+    global $pdo;
+    $stament = $pdo->query(" SELECT * FROM usuario WHERE nivel <> 'adm' ");
+    return $stament->fetchAll(PDO::FETCH_ASSOC);
+}
+
+function retornarUsuarioPorId(int $id): ?array
+{
+    global $pdo;
+    $stament = $pdo->prepare("SELEC * FROM usuario WHERE id = ? AND nivel <> 'adm'");
+    $stament->execute([$id]);
+    $usuario = $stament->fetch(PDO::FETCH_ASSOC);
+    return $usuario ? $usuario : null;
 }
