f81195a777ea2bfea421de70999a263cc73bd8d6
Felipe Pretti <felipepretti2001@gmail.com>
2024-11-24 23:13:35 -0300

    Projeto Gerenciamento de Academia - páginas de gestão de usuário, nav bar e logout

--- projetogeracademia/paginas/navbar.php
+++ projetogeracademia/paginas/navbar.php
@@ -8,4 +8,70 @@
 <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
     <div class="container-fluid">
         <a class="navbar-brand" href="/dashborad">Sistema de Gerenciamento de Acadamia</a>
-        <button class="navbar-toggler" type="button" data-bs-theme="collapse" data-bs-target=""
+        <button class="navbar-toggler" type="button" data-bs-theme="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+
+            <?php
+                if ($_SESSION['nivel' == 'adm']):
+            ?>
+                <li class="nav-item dropdawn">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Usuários
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="usuarios.php">Gerenciar</a></li>
+                    </ul>
+                </li>
+            <?php
+                endif;
+            ?>
+
+        
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Matriculas
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="matriculas.php">Gerenciar</a></li>
+          </ul>
+        </li>
+
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Planos
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="planos.php">Gerenciar</a></li>
+          </ul>
+        </li>
+
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Usuarios
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="usuarios.php">Gerenciar</a></li>
+          </ul>
+        </li>
+      </ul>
+
+        <ul class="navbar-nav ms-auto">
+            <li  class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Seja bem vindo(a)
+                    <?php
+                        if (isset($_SESSION['usuario']))
+                            echo $_SESSION['usuario'];
+                        ?>
+                </a>
+                <ul class="dropdown-menu dropdown-menu-end">
+                    <li><a class="dropdown-item" href="editar_usuario.php">Editar dados</a></li>
+                    <li><a class="dropdown-item" href="logout.php">Sair</a></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</nav>
